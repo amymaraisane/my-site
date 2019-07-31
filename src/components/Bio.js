95cc390aaeb2259023c859d48bbae4d87c9b9813
amymaraisane <amy.maraisane@gmail.com>
2019-07-31 12:42:04 -0400

    image grid is styled perfectly

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,7 +6,21 @@ export class Bio extends Component {
 
         return (
             <div className="bio">
-                
+                <div className = "imageGrid">
+                    <div className="container"> 
+                        <div class="row no-gutters">
+                            <div className="col-sm-3"><div className="card"><img alt="galaxy" src="/imgs/galaxy.jpeg"></img></div></div>
+                            <div className="col-sm-3"><div className="card"><img alt="kayak" src="/imgs/kayak.jpeg"></img></div></div>
+                            <div className="col-sm-3"><div className="card"><img alt="coffee" src="/imgs/coffee.jpeg"></img></div></div>
+                            <div className="col-sm-3"><div className="card"><img alt="presentation" src="/imgs/presentation.jpeg"></img></div></div>
+                            
+                            <div className="col-sm-3"><div className="card"><img alt="office" src="/imgs/office.jpeg"></img></div></div>
+                            <div className="col-sm-3"><div className="card"><img alt="graffiti" src="/imgs/graffiti.jpeg"></img></div></div>
+                            <div className="col-sm-3"><div className="card"><img alt="techy" src="/imgs/techy.jpeg"></img></div></div>
+                            <div className="col-sm-3"><div className="card"><img alt="trees" src="/imgs/trees.jpeg"></img></div></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,11 @@ import '../stylesheets/nav.css';
 class Nav extends Component {
     constructor(props) {
         super(props);
-        this.state = { color: "rgb(40, 100, 200"};
+        this.state = { color: "rgb(40, 100, 200)"};
+
+        setTimeout(()=>{
+            this.setState({ color: "rgb(200, 0, 50" })
+        }, 3000);
     }
 
     render() {
@@ -24,3 +28,4 @@ class Nav extends Component {
 export default Nav
 
 //because this.state.color is already inside double braces it does not need add'l
+//setState is asynchronous, callback code triggers/runs render method and updates DOM!
