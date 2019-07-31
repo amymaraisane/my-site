944ea5a41e6b015bfc1c5434a3d509dfb5968ac3
amymaraisane <amy.maraisane@gmail.com>
2019-07-31 14:19:22 -0400

    contains code to read loaded and mounted state

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 import '../stylesheets/bio.css';
+import FadeIn from './FadeIn';
 
 export class Bio extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
     render() {
 
         return (
             <div className="bio">
                 <div className = "imageGrid">
                     <div className="container"> 
-                        <div class="row no-gutters">
-                            <div className="col-sm-3"><div className="card"><img alt="galaxy" src="/imgs/galaxy.jpeg"></img></div></div>
-                            <div className="col-sm-3"><div className="card"><img alt="kayak" src="/imgs/kayak.jpeg"></img></div></div>
-                            <div className="col-sm-3"><div className="card"><img alt="coffee" src="/imgs/coffee.jpeg"></img></div></div>
-                            <div className="col-sm-3"><div className="card"><img alt="presentation" src="/imgs/presentation.jpeg"></img></div></div>
-                            
-                            <div className="col-sm-3"><div className="card"><img alt="office" src="/imgs/office.jpeg"></img></div></div>
-                            <div className="col-sm-3"><div className="card"><img alt="graffiti" src="/imgs/graffiti.jpeg"></img></div></div>
-                            <div className="col-sm-3"><div className="card"><img alt="techy" src="/imgs/techy.jpeg"></img></div></div>
-                            <div className="col-sm-3"><div className="card"><img alt="trees" src="/imgs/trees.jpeg"></img></div></div>
+                        <div className="row no-gutters">
+                            <div className="col-sm-3" id="img1"><FadeIn alt="galaxy" src="/imgs/galaxy.jpeg" /></div>
+                            <div className="col-sm-3" id="img2"><img alt="kayak" src="/imgs/kayak.jpeg"></img></div>
+                            <div className="col-sm-3" id="img3"><img alt="coffee" src="/imgs/coffee.jpeg"></img></div>
+                            <div className="col-sm-3" id="img4"><img alt="presentation" src="/imgs/presentation.jpeg"></img></div>                            
+                            <div className="col-sm-3" id="img5"><img alt="office" src="/imgs/office.jpeg"></img></div>
+                            <div className="col-sm-3" id="img6"><img alt="graffiti" src="/imgs/graffiti.jpeg"></img></div>
+                            <div className="col-sm-3" id="img7"><img alt="techy" src="/imgs/techy.jpeg"></img></div>
+                            <div className="col-sm-3" id="img8"><img alt="trees" src="/imgs/trees.jpeg"></img></div>
                         </div>
                     </div>
                 </div>
---
+++ src/components/FadeIn.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import '../stylesheets/bio.css';
+
+export class FadeIn extends Component {
+    constructor(props){
+        super(props);
+        this.state = { loaded: true};
+    };
+
+    getInitialState() {
+        this._ismounted = true;
+        return {
+            loaded: false
+        };
+    }
+
+    onImageLoad() {
+        if (this._ismounted) {
+            this.setState({ loaded: true});
+        }
+    }
+
+    componentDidMount() {
+        var imgTag = this.refs.img;
+        var imgSrc = imgTag.getAttribute('src');
+        var img = new window.Image();
+        img.onload = this.onImageLoad;
+        img.src = imgSrc;
+    }
+
+    
+    render() {
+        var { className, ...props } = this.props;
+        var imgClasses = 'img';
+        var rootClassName = className ? className + ' img' : 'img';
+        if (this.state.loaded) {
+            rootClassName += ' img-loaded';
+        }
+        return (
+            <img ref="img" {...props} className= {rootClassName} />
+        )
+    }
+}
+
+export default FadeIn
