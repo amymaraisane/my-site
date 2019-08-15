32d4dadc8fd36c7c97f9f570a84f681ff1083a4b
amymaraisane <amy.maraisane@gmail.com>
2019-08-15 12:06:33 -0400

    4 projects featured in project section with one picture only

--- src/components/Project.js
+++ src/components/Project.js
@@ -26,7 +26,7 @@ export default class Project extends Component{
         <ul>
           {about}
         </ul>
-    </div>
+    </div> 
     );
   }
 }
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,9 +6,25 @@ import PropTypes from 'prop-types';
 class ProjectList extends Component {
     static defaultProps = {
         projects: [{
+            about: ["HTML/CSS Landing page built with Bootrap 3 and updated to Bootstrap 4", "Displays modal popup window on page load"],
+            title: "Lesotho Travel Site",
+            img: "/imgs/lesotho-travel-site.png",
+            alt: "Lesotho countryside on Lesotho travel site",
+            src: "https://amymaraisane.github.io/RGB-Guessing-Game/"
+            
+        },
+        {
+            about: ["My first vanilla JavaScript project", "RGB color matching game with click listeners and easy/hard modes"],
+            title: "Color Craze Game",
+            img: "/imgs/color-craze.png",
+            alt: "Color Craze matching game site",
+            src: "https://amymaraisane.github.io/RGB-Guessing-Game/"
+            
+        },
+        {
             about: ["Uses Node.js, Express, and MongoDB", "97 Lighthouse Performance Rating"],
             title: "Music Forum",
-            img: "/imgs/forum-landingpage.png",
+            img: "/imgs/music-forum.png",
             alt: "Music Forum Landing Page showing record player",
             src: "https://vast-sierra-13998.herokuapp.com/"
             
@@ -16,7 +32,7 @@ class ProjectList extends Component {
         {
             about: ["Node.js, Express, and MongoDB", "Custom JSON API and RESTful routing"],
             title: "Netflix Watchlist",
-            img: "/imgs/netflix-watchlist.png",
+            img: "/imgs/netflix-watchlist-full.png",
             alt: "Red Netflix Watchlist logo", 
             src: "https://pacific-oasis-25527.herokuapp.com/"
         }]
