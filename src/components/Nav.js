d498fb26773874bf236601945991f915d6d6d55c
amymaraisane <amy.maraisane@gmail.com>
2019-08-15 12:38:25 -0400

    style navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,21 +2,13 @@ import React, { Component } from 'react';
 import '../stylesheets/nav.css';
 
 class Nav extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { color: "rgb(40, 100, 200)"};
-
-        setTimeout(()=>{
-            this.setState({ color: "rgb(200, 0, 50" })
-        }, 3000);
-    }
 
     render() {
         return (
             <header>
-                <h2>AM</h2>
+                <h2>Amy Maraisane</h2>
                 <nav>
-                    <li><a href="#" style= {{color: this.state.color}}>About</a></li>
+                    <li><a href="#">About</a></li>
                     <li><a href="#project-list">Projects</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </nav>
