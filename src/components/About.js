e25255d6fc20eb3d1e83269f73d793986e39be01
Amy Maraisane <amy.maraisane@7factor.io>
2021-01-16 18:40:45 -0500

    update 2 words

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export class About extends Component {
                     <div className= "row flex">
                         <h3>About</h3>
                         <p>
-                            After starting my career in employee training and development at a staffing company, 
+                            After starting my career in employee development at a staffing company,
                             I soon realized it was time for me to level up and pursue my interest in web development. Rather than enrolling
                             in a bootcamp, I decided to design my own. 600 hours of Udemy courses, pair programming, podcasts, and 
                             community-based learning later, I know I can learn anything. Let's talk about how I can help you build your dream.
